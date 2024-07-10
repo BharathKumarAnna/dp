f580bb731749fb92dbecefb31dd1b516f21ee65e
Bharath kumar Anna <bharathkumaranna10@gmail.com>
2024-07-10 22:20:12 +0530

    program_1

---
+++ pg1_fib.py
@@ -0,0 +1,13 @@
+def fib(n,dp):
+    if n==0:
+        return 0
+    elif n == 1:
+        return 1
+    elif dp[n]!=-1:
+        return dp[n]
+    else:
+        return fib(n-1,dp)+fib(n-2,dp)
+
+n = int(input("Enter the nth value: "))       
+dp = [-1 for _ in range(n+1)]        
+print(fib(n,dp))
