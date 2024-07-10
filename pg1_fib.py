0d34c9bc59837a9a487d986f876c8f75facc09af
Bharath kumar Anna <bharathkumaranna10@gmail.com>
2024-07-10 22:44:30 +0530

    top_down and Bottom_up approach

--- pg1_fib.py
+++ pg1_fib.py
@@ -1,3 +1,7 @@
+# Top Down approach with DP.
+# Memoisation
+# recursion
+'''
 def fib(n,dp):
     if n==0:
         return 0
@@ -11,7 +15,17 @@ def fib(n,dp):
 n = int(input("Enter the nth value: "))       
 dp = [-1 for _ in range(n+1)]        
 print(fib(n,dp))
+'''
 
-# Top Down approach with DP.
-# Memorization
-# recursion
+
+# Bottom Up approach with DP.
+# Tabulation
+# Iteration
+
+dp = [0]*(101)
+dp[1] = 1
+for i in range(2,101):
+    dp[i] = dp[i-1]+dp[i-2]
+    
+print(dp[7])
+
